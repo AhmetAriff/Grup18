abae66336a6982595742fdf6d70ede3508b48ae5
Abdulkadir_USTA <usta1829@gmail.com>
2023-12-27 09:29:33 +0300

    deneme 2

--- src/asd/Main.java
+++ src/asd/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("ariff");
-
+		
+		System.out.println("usta deneme");
+		System.out.println("usta deneme");
+		System.out.println("usta deneme");
+		System.out.println("usta deneme");
 	}
 
 }
