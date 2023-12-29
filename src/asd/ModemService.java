d5b060e9149f7d32278f9c4fd74adc46f0133a91
Tuncer Alevli <103581716+tunceralv@users.noreply.github.com>
2023-12-29 15:47:51 +0300

    Update ModemService.java

--- src/asd/ModemService.java
+++ src/asd/ModemService.java
@@ -2,8 +2,22 @@ package asd;
 
 public class ModemService {
 
-	public ModemService() {
-		// TODO Auto-generated constructor stub
-	}
+    private final int sistemdekiModemSayisi = 1;
+    private int kullanimdakiModemSayisi = 0;
+    public int getsistemdekiModemSayisi(){
+        return this.sistemdekiModemSayisi;
+    }
+    public int getMusaitModemSayisi(){
+        return sistemdekiModemSayisi - kullanimdakiModemSayisi;
+    }
+    public boolean modemMusaitMi(Process process){
+        return process.getModems() <= getMusaitModemSayisi();
+    }
+    public void modemiKullan(Process process){
+        kullanimdakiModemSayisi += process.getModems();
+    }
+    public void modemiSerbestBirak(Process process){
+        kullanimdakiModemSayisi -=process.getModems();
+    }
 
 }
