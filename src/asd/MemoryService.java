537edf65185ffaef5e192556b904ac3d647f309c
Tuncer Alevli <103581716+tunceralv@users.noreply.github.com>
2023-12-29 15:48:18 +0300

    Update MemoryService.java

--- src/asd/MemoryService.java
+++ src/asd/MemoryService.java
@@ -2,8 +2,18 @@ package asd;
 
 public class MemoryService {
 
-	public MemoryService() {
-		// TODO Auto-generated constructor stub
-	}
+	MemoryService(){
+        memory = new int[1024];
+        for(int i = 0;i<1024;i++){
+            memory[i] = 0;
+        }
+    }
+    private int [] memory;
+    private final int  maximumMemoryKapasitesiForUserJob = 960;
+    private final int  maximumMemoryKapasitesiForRealTime = 64;
+
+    public boolean memoryMusaitMi(Process process) {
+        return getBestFitIndex(process) != -1;
+    }
 
 }
